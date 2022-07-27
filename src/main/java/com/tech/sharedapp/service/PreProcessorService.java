dcf4f44ce2a189c3f6417b8d40dd3144d9032f7d
Aakash Kumar <aakashthesid@gmail.com>
2022-07-28 00:12:39 +0530

    Stable code, getting invoked when used from the other app

--- src/main/java/com/tech/sharedapp/service/PreProcessorService.java
+++ src/main/java/com/tech/sharedapp/service/PreProcessorService.java
@@ -1,21 +1,27 @@
 package com.tech.sharedapp.service;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class PreProcessorService {
+public class PreProcessorService implements ApplicationListener<ContextRefreshedEvent> {
 
     private final Environment environment;
+    private static Logger logger = LogManager.getLogger(PreProcessorService.class);
 
-    public void initialiseAudit() {
-        System.out.println("##### Ruuning pre processor ##"+ environment.getProperty("db.schema")+ " #####");
-        System.out.println("##### Ruuning pre processor ##"+ environment.getProperty("db.audit.table")+ " #####");
-        System.out.println("##### Ruuning pre processor ##"+ environment.getProperty("db.audit.mapping_table")+ " #####");
-        System.out.println("##### Ruuning pre processor ##"+ environment.getProperty("db.audit.audit_mode")+ " #####");
-        System.out.println("##### Ruuning pre processor ##"+ environment.getProperty("db.audit.audit_retention")+ " #####");
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        logger.info("##### Ruuning pre processor ##" + environment.getProperty("db.schema") + " #####");
+        logger.info("##### Ruuning pre processor ##" + environment.getProperty("db.audit.table") + " #####");
+        logger.info("##### Ruuning pre processor ##" + environment.getProperty("db.audit.mapping_table") + " #####");
+        logger.info("##### Ruuning pre processor ##" + environment.getProperty("db.audit.audit_mode") + " #####");
+        logger.info("##### Ruuning pre processor ##" + environment.getProperty("db.audit.audit_retention") + " #####");
+
     }
-
 }
